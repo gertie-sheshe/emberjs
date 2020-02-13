1edae6576c0e6311fcc99864f61097ff5dcbd829
gertie-sheshe <gertrude.nyenyeshi@gmail.com>
2020-02-13 18:15:54 +0300

    add complete task count controller functionality

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,4 +1,10 @@
-import Controller from '@ember/controller';
+import Controller from "@ember/controller";
 
 export default class IndexController extends Controller {
+  get results() {
+    let dataResults = 0;
+    dataResults = this.model.filter(task => task.isComplete === true);
+
+    return dataResults.length;
+  }
 }
